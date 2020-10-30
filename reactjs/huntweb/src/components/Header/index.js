cb8f39c9309a1a40414fc3b6977171395946287e
Carla Barden <cdbarden@inf.ufsm.br>
2020-10-30 14:55:24 -0300

    Corrigindo hierarquia de diretórios

--- reactjs/huntweb/src/components/Header/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import "./styles.css";
-
-//stateless components -> função em vez de classe
-const Header = () => (
-   <header id="main-header">JSHunt</header> 
-);
-
-export default Header;
--- reactjs/huntweb/src/services/api.js
+++
@@ -1,8 +0,0 @@
-// serviços externos
-import axios from 'axios';
-
-const api = axios.create({ 
-    baseURL: 'https://rocketseat-node.herokuapp.com/api'
-});
-
-export default api;
